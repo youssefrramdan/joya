d6fec0a0b518db95afe26b250c0c8ebd58c4c5d2
youssefrramdan <youfahmed353@gmail.com>
2024-11-01 17:08:24 +0200

    add3

--- src/app/(Pages)/Projects/Features/[buyPropertyId]/_components/BuyPropertyImages.tsx
+++ src/app/(Pages)/Projects/Features/[buyPropertyId]/_components/BuyPropertyImages.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import Image as NextImage from "next/image";  // إعادة التسمية لتجنب التعارض
+import Image from "next/image"; 
 import ImageSlider from "./ImageSlider";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 
@@ -18,47 +18,54 @@ function BuyPropertyImages({
     <div className="col-lg-6 col-md-6 col-sm-12">
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
         {/* First Row of Images */}
-        <div className="lg:col-span-1">
-          <div className="image mb-5">
-            <a href={images[0]?.secure_url ?? ""} data-fancybox="images">
-              <NextImage
-                src={images[0]?.secure_url ?? ""}
-                alt={images[0]?.alt_text ?? "Image"}
-                width={500}
-                height={500}
-                className="img-responsive w-full h-auto object-cover"
-              />
-            </a>
+        {images[0]?.secure_url && (
+          <div className="lg:col-span-1">
+            <div className="image mb-5">
+              <a href={images[0].secure_url} data-fancybox="images">
+                <Image
+                  src={images[0].secure_url}
+                  alt={images[0].alt_text || "Image"}
+                  width={500}
+                  height={500}
+                  className="img-responsive w-full h-auto object-cover"
+                />
+              </a>
+            </div>
           </div>
-        </div>
-        <div className="lg:col-span-1">
-          <div className="image mb-5">
-            <a href={images[1]?.secure_url} data-fancybox="images">
-              <NextImage
-                src={images[1]?.secure_url ?? ""}
-                alt={images[1]?.alt_text ?? "Image"}
-                width={500}
-                height={500}
-                className="img-responsive w-full h-auto object-cover"
-              />
-            </a>
+        )}
+        
+        {images[1]?.secure_url && (
+          <div className="lg:col-span-1">
+            <div className="image mb-5">
+              <a href={images[1].secure_url} data-fancybox="images">
+                <Image
+                  src={images[1].secure_url}
+                  alt={images[1].alt_text || "Image"}
+                  width={500}
+                  height={500}
+                  className="img-responsive w-full h-auto object-cover"
+                />
+              </a>
+            </div>
           </div>
-        </div>
+        )}
 
         {/* Second Row of Images */}
-        <div className="lg:col-span-2">
-          <div className="image mb-5">
-            <a href={images[2]?.secure_url} data-fancybox="images">
-              <NextImage
-                src={images[2]?.secure_url ?? ""}
-                alt={images[2]?.alt_text ?? "Image"}
-                width={500}
-                height={500}
-                className="img-responsive w-full h-auto object-cover"
-              />
-            </a>
+        {images[2]?.secure_url && (
+          <div className="lg:col-span-2">
+            <div className="image mb-5">
+              <a href={images[2].secure_url} data-fancybox="images">
+                <Image
+                  src={images[2].secure_url}
+                  alt={images[2].alt_text || "Image"}
+                  width={500}
+                  height={500}
+                  className="img-responsive w-full h-auto object-cover"
+                />
+              </a>
+            </div>
           </div>
-        </div>
+        )}
 
         {/* Show all photos */}
         <div className="lg:col-span-2">
--- src/app/(Pages)/Projects/Features/[buyPropertyId]/_components/ImageSlider.tsx
+++ src/app/(Pages)/Projects/Features/[buyPropertyId]/_components/ImageSlider.tsx
@@ -1,52 +1,53 @@
-// components/ImageSlider.js
+"use client";
 import Slider from "react-slick";
 import Image from "next/image";
-import { Image as ImageType } from "../page";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+
+// تعريف نوع الصور هنا لتجنب التعقيدات المحتملة
+type ImageType = {
+  secure_url: string;
+  alt_text: string;
+};
 
 const ImageSlider = ({ images }: { images: ImageType[] }) => {
   console.log('images inside image slider', images);
 
-  const NextArrow = ({ onClick }: { onClick: () => void }) => {
-    return (
-      <div
-        className="absolute top-1/2 right-[-28%] transform -translate-y-1/2 bg-[#041d1a] p-1 rounded-full shadow-md cursor-pointer"
-        onClick={onClick}
+  const NextArrow = ({ onClick }: { onClick: () => void }) => (
+    <div
+      className="absolute top-1/2 right-[-28%] transform -translate-y-1/2 bg-[#041d1a] p-1 rounded-full shadow-md cursor-pointer"
+      onClick={onClick}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="2"
+        stroke="currentColor"
+        className="w-4 h-4 text-[#faf8f7]"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="2"
-          stroke="currentColor"
-          className="w-4 h-4 text-[#faf8f7]"
-        >
-          <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
-        </svg>
-      </div>
-    );
-  };
+        <path strokeLinecap="round" strokeLinejoin="round" d="M9 5l7 7-7 7" />
+      </svg>
+    </div>
+  );
 
-  const PrevArrow = ({ onClick }: { onClick: () => void }) => {
-    return (
-      <div
-        className="absolute top-1/2 left-[-28%] transform -translate-y-1/2 bg-[#041d1a] p-1 rounded-full shadow-md cursor-pointer z-50"
-        onClick={onClick}
+  const PrevArrow = ({ onClick }: { onClick: () => void }) => (
+    <div
+      className="absolute top-1/2 left-[-28%] transform -translate-y-1/2 bg-[#041d1a] p-1 rounded-full shadow-md cursor-pointer z-50"
+      onClick={onClick}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="2"
+        stroke="currentColor"
+        className="w-4 h-4 text-[#faf8f7]"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="2"
-          stroke="currentColor"
-          className="w-4 h-4 text-[#faf8f7]"
-        >
-          <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
-        </svg>
-      </div>
-    );
-  };
+        <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
+      </svg>
+    </div>
+  );
 
   const settings = {
     centerMode: true,
@@ -64,15 +65,17 @@ const ImageSlider = ({ images }: { images: ImageType[] }) => {
     <div className="w-full h-full max-w-[30rem] md:max-w-6xl mx-auto">
       <Slider {...settings}>
         {images.map((img, index) => (
-          <div key={index} className="flex justify-center">
-            <Image
-              src={img.secure_url}
-              alt={`Slide ${index}`}
-              width={800}
-              height={500}
-              className="w-full h-full object-cover rounded-md mx-auto"
-            />
-          </div>
+          img.secure_url ? ( // التحقق من وجود secure_url
+            <div key={index} className="flex justify-center">
+              <Image
+                src={img.secure_url}
+                alt={img.alt_text || `Slide ${index}`}
+                width={800}
+                height={500}
+                className="w-full h-full object-cover rounded-md mx-auto"
+              />
+            </div>
+          ) : null
         ))}
       </Slider>
     </div>
